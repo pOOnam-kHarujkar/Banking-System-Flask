90f52ab413fc276e18e14e05eee7fd3786f593c3
poonam <poonamkharujkar7890@gmail.com>
2024-06-17 12:08:31 +0530

    banking system app

--- banking_system/app/__init__.py
+++ banking_system/app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_pymongo import PyMongo
-# from flask_jwt_extended import JWTManager
+
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
